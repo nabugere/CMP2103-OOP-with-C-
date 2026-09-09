78ea865c859369c412f16fba7646406250a480da
nabugere <adriannabugere@gmail.com>
2026-09-09 15:06:01 +0300

    Add BMI calculation and classification to bmi.cpp

--- Chapter_03/bmi.cpp
+++ Chapter_03/bmi.cpp
@@ -13,3 +13,31 @@
 // Enter height in inches: 70
 // BMI is 20.95
 // Normal
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double weightPounds, heightInches;
+    cout << "Enter weight in pounds: ";
+    cin >> weightPounds;
+    cout << "Enter height in inches: ";
+    cin >> heightInches;
+    const double POUND_TO_KG = 0.45359237;
+    const double INCH_TO_METER = 0.0254;
+    double weightKg = weightPounds * POUND_TO_KG;
+    double heightMeters = heightInches * INCH_TO_METER;
+    double bmi = weightKg / (heightMeters * heightMeters);
+    cout << "BMI is " << fixed << setprecision(2) << bmi << endl;
+    if (bmi < 18.5)
+        cout << "Underweight" << endl;
+    else if (bmi < 25.0)
+        cout << "Normal" << endl;
+    else if (bmi < 30.0)
+        cout << "Overweight" << endl;
+    else
+        cout << "Obese" << endl;
+
+    return 0;
+}
